3b3fd12069e9be770add9c044f5a1d1dea969cb9
elzero <24859@qq.com>
2017-04-10 00:41:00 +0800

    解决 ... 不能通过 babel 问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "[name].js",
-        path: "./dist"
+        chunkFilename: "[name].js",
+        path: "./dist",
+        publicPath: "./dist",
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
@@ -14,9 +19,20 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react', "stage-0", "env"]
                 }
-            }
+            },
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.less$/, loader: 'style!css!less' },
+            /*{
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style", "css")
+            }*/
         ]
-    }
+    },
+    plugins: [
+        // new ExtractTextPlugin("css/[name].css", {
+        //     allChunks: true
+        // })
+       /*new HtmlWebpackPlugin()*/]
 };
